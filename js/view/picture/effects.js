0f9242efdf74d85051c2fd3c0e1780a6e4235c8d
Jempasam <samueldemont83@gmail.com>
2024-11-29 11:27:51 +0100

    Slime + Better Sayan + Slimy effect + Correction

--- js/view/picture/effects.js
+++ js/view/picture/effects.js
@@ -1,7 +1,40 @@
 import { Picture } from "../../cardgame/icon/Picture.js";
 
-/** @type { {[name:string]:function(Picture):Picture} } */
+/** @type { {[name:string]:function(Picture,...any|undefined):Picture} } */
 const effects ={
+
+    normal(it){ return it },
+
+    drippy(input, dx=0, dy=1, lens={0:2, 1:1, 2:1, 3:1}, only_ins={}){
+        if(dx!=0)input = effects.fit_to(input, 0, -Math.sign(dx))
+        if(dy!=0)input = effects.fit_to(input, 1, -Math.sign(dy))
+        const output = input.clone()
+        for(const [x,y] of input.indexes()){
+            const depth = input.get_depth(x,y)
+            const material = input.get_material_index(x,y)
+            if(depth==-1)continue
+            let distance_to_center = Math.max(Math.abs(Math.floor(input.width/2)-x), Math.abs(Math.ceil(input.width/2)-1-x))
+            console.log(x,distance_to_center)
+            if(distance_to_center%2!=material%2)continue
+            const len = lens[material] ?? 0
+            const only_in = only_ins[material] ?? false
+            for(let l=1; l<=len; l++){
+                let xx=x+dx*l, yy=y+dy*l
+                if(
+                    input.contains(xx,yy)
+                    && (
+                        (input.get_material_index(xx,yy)!=material && (input.contains(xx+dx,yy+dy) && input.get_material_index(xx+dx,yy+dy)==input.get_material_index(xx,yy) && input.get_depth(xx+dx,yy+dy)!=-1)) // Drip on material
+                        || (input.get_depth(xx,yy)==-1 && !only_in) // Drip on empty
+                    )
+                ){
+                    output.set_depth(xx, yy, depth)
+                    output.set_material_index(xx, yy, input.get_material_index(x,y))
+                }
+                else break
+            }
+        }
+        return output
+    },
 
     look_right(it, dx=-1, dy=0){
         const ret = it.clone()
@@ -38,12 +71,9 @@ const effects ={
     look_left(it){ return effects.look_right(it, 1, 0) },
     look_up(it){ return effects.look_right(it, 0, 1) },
     look_down(it){ return effects.look_right(it, 0, -1) },
-    
-
-    normal(it){ return it },
 
     sayan(it){
-        const ret = it.clone()
+        let ret = it.clone()
         ret.materials[0].color[0]= 1.0
         ret.materials[0].color[1]= 0.8
         ret.materials[0].color[2]= 0.0
@@ -54,24 +84,26 @@ const effects ={
         ret.materials[2].color[2]= 1.0
         ret.materials[2].light+= 0.4
 
-        for(let x=0; x<ret.width; x++)if(x%3==0){
-            for(let y=0; y<ret.height; y++){
-                const depth = ret.get_depth(x,y)
-                if(depth==-1)continue
-                if(y-1>=0){
-                    const color = ret.get_material_index(x,y)
-                    ret.set_depth(x,y-1, depth)
-                    ret.set_material_index(x,y-1, color)
-                }
-                if(y-2>=0){
-                    const color = ret.get_material_index(x,y)
-                    ret.set_depth(x,y-2, depth)
-                    ret.set_material_index(x,y-2, color)
-                }
-                break
-            }
-        }
+        ret = effects.drippy(ret, 0, -1, {0:2, 1:1, 2:1}, {1:true})
+        return ret
+    },
 
+    slimy(it){
+        let ret = it.clone()
+        ret.materials[0].color = lerp_colors(it.materials[0].color, [0.2, 1.0, 0.2], 0.5)
+        ret.materials[0].alpha = lerp(it.materials[0].alpha, 0.8, 0.4)
+        ret.materials[0].reflection = lerp(it.materials[1].reflection, 0.5, 0.4)
+
+        ret.materials[1].color = lerp_colors(it.materials[1].color, [0.2, 1.0, 0.2], 0.3)
+        ret.materials[1].reflection = lerp(it.materials[1].reflection, 0.5, 0.3)
+
+        ret.materials[2].color = lerp_colors(it.materials[2].color, [0.2, 1.0, 0.2], 0.2)
+        ret.materials[2].reflection = lerp(it.materials[2].reflection, 0.5, 0.2)
+
+        ret.materials[3].color = lerp_colors(it.materials[3].color, [0.2, 1.0, 0.2], 0.2)
+        ret.materials[3].reflection = lerp(it.materials[3].reflection, 0.5, 0.1)
+
+        ret = effects.drippy(ret, 0, 1, {0:2, 1:1, 2:1, 3:1})
         return ret
     },
 
@@ -171,6 +203,91 @@ const effects ={
         return ret
     },
 
+    /**
+     * Move all the pixel to to make, the picture, close fit a side if possible by keepeing a space of at least one pixel. 
+     **/
+    fit_to(input, axes=1, direction=1){
+        direction = -direction
+
+        const output = input.clone()
+        let dims = [input.width, input.height]
+        let axes2 = (axes+1)%2
+        let len = 0
+
+        // Get the amount of movement possible
+        let maxi = direction==1 ? dims[axes]-1 : -1
+        let mini = direction==1 ? 0 : dims[axes]-1
+        let diri = direction==1 ? 1 : -1
+        loop: for(let i=mini; i!=maxi; i+=diri){
+            for(let j=0; j<dims[axes2]; j++){
+                let coords = [0,0]
+                coords[axes] = i
+                coords[axes2] = j
+                if(input.get_depth(...coords)!=-1) break loop
+            }
+            len++
+        }
+        console.log(len)
+
+        // At least one empty space before border
+        len--
+
+        // Move the pixels
+        for(let i=0; i<dims[axes]; i++){
+            for(let j=0; j<dims[axes2]; j++){
+                let output_coords = [0,0]
+                let input_coords = /** @type {[Number,number]} */ ([0,0])
+                output_coords[axes] = i
+                output_coords[axes2] = j
+                input_coords[axes] = i + len * direction
+                input_coords[axes2] = j
+                if(input.contains(...input_coords)){
+                    output.set_depth(...output_coords, input.get_depth(...input_coords))
+                    output.set_material_index(...output_coords, input.get_material_index(...input_coords))
+                }
+                else{
+                    output.set_depth(...output_coords, -1)
+                }
+            }
+        }
+
+        return output
+    },
+
 
 }
+
+/**
+ * Linear interpolation between two arrays.
+ * @param {Array<number>} a
+ * @param {Array<number>} b
+ * @param {number} t
+ * @returns {Array<number>}
+ */
+function lerp_arrays(a,b,t){
+    return a.map((v,i)=>v*(1-t)+b[i]*t)
+}
+
+/**
+ * Linear interpolation between two colors.
+ * @param {[number,number,number]} a
+ * @param {[number,number,number]} b
+ * @param {number} t
+ * @returns {[number,number,number]}
+ */
+function lerp_colors(a,b,t){
+    return /** @type {[number,number,number]} */ (lerp_arrays(a,b,t))
+}
+
+/**
+ * Linear interpolation between two values.
+ * @param {number} a
+ * @param {number} b
+ * @param {number} t
+ * @returns {number}
+ */
+function lerp(a,b,t){
+    return a*(1-t)+b*t
+}
+
 export default effects;
